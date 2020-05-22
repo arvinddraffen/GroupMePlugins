d868ff584e2ae45e9dd68e15e17a899233f2f910
Arvind Draffen <arvinddraffen@gmail.com>
2020-05-21 21:27:44 -0500

    Written to be entirely in C# and with latest GroupMe Desktop Client plugin API

--- LetterSwapPlugin/LetterSwapPlugin.cs
+++ LetterSwapPlugin/LetterSwapPlugin.cs
@@ -1,56 +1,233 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using GroupMeClientPlugin.MessageCompose;
-using Newtonsoft.Json.Linq;
 
-namespace LetterSwapPlugin
+namespace EffectDemo
 {
     public class LetterSwapPlugin : GroupMeClientPlugin.PluginBase, IMessageComposePlugin
     {
-        public string EffectPluginName => "Letter Swap";
+        public string EffectPluginName => this.PluginDisplayName;
 
-        public override string PluginDisplayName => "Letter Swap Plugin";
+        public override string PluginDisplayName => "Letter Swap";
 
         public override string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-        public async Task<MessageSuggestions> ProvideOptions(string typedMessage)
+        public override Version ApiVersion => new Version(2, 0, 0);
+
+        public Task<MessageSuggestions> ProvideOptions(string typedMessage)
         {
-            var result = new MessageSuggestions();
-
-            await Task.Run(() => DoLetterSwap(typedMessage, result));
-
-            return result;
+            var results = new MessageSuggestions();
+            //results.TextOptions.Add("DEMO: " + typedMessage.ToUpper());
+            results.TextOptions.Add(this.DoLetterSwap(typedMessage));
+            results.TextOptions.Add(this.DoLetterSwapReverse(typedMessage));
+            results.TextOptions.Add(this.DoGSSwap(typedMessage));
+            results.TextOptions.Add(this.DoBlahsBlahforBlah(typedMessage));
+            results.TextOptions.Add(this.DoBlaherFile(typedMessage));
+            return Task.FromResult(results);
         }
 
-        private void DoLetterSwap(string text, MessageSuggestions result)
+        private string DoLetterSwap(string text)
         {
-            var proc = new Process
+            List<string> leadingCharacters = new List<string>();
+            List<char> vowels = new List<char>()
             {
-                StartInfo = new ProcessStartInfo
+                'a', 'e', 'i', 'o', 'u', 'y'
+            };
+            string[] originalWords = text.Split(' ');
+            List<string> words = new List<string>();
+            foreach (string word in originalWords)
+            {
+                if (!string.IsNullOrEmpty(word))
                 {
-                    FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "PhraseManipulation-plugin.exe"),
-                    Arguments = text,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
+                    words.Add(word);
                 }
+            }
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (vowels.Contains(char.ToLower(word[i])))
+                    {
+                        if (char.ToLower(word[i]) == 'y')
+                        {
+                            if (i == word.Length - 1)
+                            {
+                                leadingCharacters.Add(word.Substring(0, i));
+                            }
+                            else
+                            {
+                                if (vowels.Contains(char.ToLower(word[i + 1])))
+                                {
+                                    leadingCharacters.Add(word[i].ToString());
+                                    break;
+                                }
+                                else
+                                {
+                                    leadingCharacters.Add(word.Substring(0, i));
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            leadingCharacters.Add(word.Substring(0, i));
+                            break;
+                        }
+                    }
+                    else if (vowels.Contains(char.ToLower(word[0])))
+                    {
+                        leadingCharacters.Add(word[0].ToString());
+                        break;
+                    }
+                    else if (i == word.Length - 1)
+                    {
+                        leadingCharacters.Add(word);
+                        break;
+                    }
+                }
+            }
+
+            int startIndex, endIndex;
+            for (int i = 0; i < words.Count; i++)
+            {
+                startIndex = leadingCharacters[i].Length;
+                endIndex = words[i].Length - startIndex;
+                words[i] = words[i].Substring(startIndex, endIndex);
+                if (i == 0)
+                {
+                    words[i] = leadingCharacters[leadingCharacters.Count - 1] + words[i];
+                }
+                else
+                {
+                    words[i] = leadingCharacters[i - 1] + words[i];
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+        private string DoLetterSwapReverse(string text)
+        {
+            List<string> leadingCharacters = new List<string>();
+            List<char> vowels = new List<char>()
+            {
+                'a', 'e', 'i', 'o', 'u'
             };
 
-            proc.Start();
-
-            var json = proc.StandardOutput.ReadLine();
-            Debug.WriteLine(json);
-            if (!string.IsNullOrEmpty(json))
+            string[] originalWords = text.Split(' ');
+            List<string> words = new List<string>();
+            foreach (string word in originalWords)
             {
-                var effects = JObject.Parse(json);
-                foreach (System.Collections.Generic.KeyValuePair<string, JToken> effect in effects)
+                if (!string.IsNullOrEmpty(word))
                 {
-                    Debug.WriteLine(effect);
-                    result.TextOptions.Add((string)effect.Value);
+                    words.Add(word);
                 }
+            }
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (vowels.Contains(char.ToLower(word[i])))
+                    {
+                        if (char.ToLower(word[i]) == 'y')
+                        {
+                            if (i == word.Length - 1)
+                            {
+                                leadingCharacters.Add(word.Substring(0, i));
+                            }
+                            else
+                            {
+                                if (vowels.Contains(char.ToLower(word[i + 1])))
+                                {
+                                    leadingCharacters.Add(word[i].ToString());
+                                    break;
+                                }
+                                else
+                                {
+                                    leadingCharacters.Add(word.Substring(0, i));
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            leadingCharacters.Add(word.Substring(0, i));
+                            break;
+                        }
+                    }
+                    else if (vowels.Contains(char.ToLower(word[0])))
+                    {
+                        leadingCharacters.Add(word[0].ToString());
+                        break;
+                    }
+
+                    else if (i == word.Length - 1)
+                    {
+                        leadingCharacters.Add(word);
+                        break;
+                    }
+                }
+            }
+
+            int startIndex, endIndex;
+            for (int i = 0; i < words.Count; i++)
+            {
+                startIndex = leadingCharacters[i].Length;
+                endIndex = words[i].Length - startIndex;
+                words[i] = words[i].Substring(startIndex, endIndex);
+                words[i] = leadingCharacters[(i + 1) % leadingCharacters.Count] + words[i];
+            }
+            return string.Join(" ", words);
+        }
+
+        private string DoGSSwap(string text)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach(char letter in text)
+            {
+                if (letter == 'S')
+                {
+                    builder.Append('G');
+                }
+                else if (letter == 's')
+                {
+                    builder.Append('g');
+                }
+                else if (letter == 'G')
+                {
+                    builder.Append('S');
+                }
+                else if (letter == 'g')
+                {
+                    builder.Append('s');
+                }
+                else
+                {
+                    builder.Append(letter);
+                }
+            }
+            return builder.ToString();
+        }
+
+        private string DoBlahsBlahforBlah(string text)
+        {
+            return text + "'s " + text + " for " + text;
+        }
+
+        private string DoBlaherFile(string text)
+        {
+            if (text.EndsWith("e"))
+            {
+                return text + "r file";
+            }
+            else
+            {
+                return text + "er file";
             }
         }
     }
